a4f4d7f367efd9d9f5aa25966320eb179f8b51e3
Connor Sinnott <connorsinnott@gmail.com>
2018-06-30 17:38:32 -0700

    Added flow to react

--- react/src/index.js
+++ react/src/index.js
@@ -1,8 +1,13 @@
+// @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const root: ?Element = document.getElementById('root');
+
+if (root != null) {
+    ReactDOM.render(<App/>, root);
+    registerServiceWorker();
+}
