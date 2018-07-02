f6b7f57855f10632addee3d3e285a7ba3a1b57b2
Jefferson Eagley <jefferson.eagley@gmail.com>
2018-07-02 11:22:03 -0700

    temp comment

--- react/src/_redux/reducer/index.js
+++ react/src/_redux/reducer/index.js
@@ -3,14 +3,14 @@ import {combineReducers} from 'redux';
 /**
  * Reducers
  */
-import reducer_Forms from './reducer_Form';
-import reducer_Navigation from './reducer_Nav';
+//import reducer_Forms from './reducer_Form';
+//import reducer_Navigation from './reducer_Nav';
 import reducer_UI from './reducer_UI';
-import reducer_Archive from './reducer_Archive';
+//import reducer_Archive from './reducer_Archive';
 
 export default combineReducers({
-  formsStore: reducer_Forms,
-  navigationStore: reducer_Navigation,
+//  formsStore: reducer_Forms,
+//  navigationStore: reducer_Navigation,
   uiStore: reducer_UI,
-  archiveStore: reducer_Archive,
+//  archiveStore: reducer_Archive,
 });
