34b65fb01baa296661c2043cf546771ebe76b7e3
Jefferson Eagley <jefferson.eagley@gmail.com>
2018-07-02 11:11:31 -0700

    imported existing redux helpers

---
+++ react/src/_core/redux/actionStatus.js
@@ -0,0 +1,6 @@
+export const UNINITIALIZED = 'UNINITIALIZED';
+export const STARTED = 'STARTED';
+export const COMPLETE = 'COMPLETE';
+export const ERROR = 'ERROR';
+export const STOPPED = 'STOPPED';
+export const CANCELLED = 'CANCELLED';
