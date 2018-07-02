93b15ce01f04c9dc2f456f131c0fd0576354c752
Jefferson Eagley <jefferson.eagley@gmail.com>
2018-07-02 11:22:25 -0700

    navigation rename

--- react/src/_redux/reducer/index.js
+++ react/src/_redux/reducer/index.js
@@ -4,13 +4,13 @@ import {combineReducers} from 'redux';
  * Reducers
  */
 //import reducer_Forms from './reducer_Form';
-//import reducer_Navigation from './reducer_Nav';
+//import reducer_Nav from './reducer_Nav';
 import reducer_UI from './reducer_UI';
 //import reducer_Archive from './reducer_Archive';
 
 export default combineReducers({
 //  formsStore: reducer_Forms,
-//  navigationStore: reducer_Navigation,
+//  navStore: reducer_Nav,
   uiStore: reducer_UI,
 //  archiveStore: reducer_Archive,
 });
