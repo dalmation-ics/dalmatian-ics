d5d3b2096846d38306da424ec4a8e5111fc83f81
Jefferson Eagley <jefferson.eagley@gmail.com>
2018-07-03 20:31:28 -0700

    added new reducers in and refactor

--- react/src/_redux/reducer/index.js
+++ react/src/_redux/reducer/index.js
@@ -3,14 +3,14 @@ import {combineReducers} from 'redux';
 /**
  * Reducers
  */
-//import reducer_Forms from './reducer_Form';
+//import reducer_FormTemplate from './reducer_FormTemplate';
 import reducer_Nav from './reducer_Nav';
 import reducer_UI from './reducer_UI';
-//import reducer_Archive from './reducer_Archive';
+import reducer_Archive from './reducer_Archive';
 
 export default combineReducers({
-//  formsStore: reducer_Forms,
+//  formTemplateStore: reducer_FormTemplate,
   navStore: reducer_Nav,
   uiStore: reducer_UI,
-//  archiveStore: reducer_Archive,
+  archiveStore: reducer_Archive,
 });
