d8315fd24d95a29fa446cd425b7da6435a056b6a
Jefferson Eagley <jefferson.eagley@gmail.com>
2018-07-03 20:41:52 -0700

    app cleanup

--- react/src/component/AppRedux/App.js
+++ react/src/component/AppRedux/App.js
@@ -1,25 +1,23 @@
 // @flow
 import React, {Component} from 'react';
-import logo from '../../logo.svg';
 import './App.css';
 
 type Props = {};
 
 class App extends Component<Props> {
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to
-                    reload.
-                </p>
-            </div>
-        );
-    }
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to
+            reload.
+          </p>
+        </div>
+    );
+  }
 }
 
 export default App;
