a6e34756f725ec9b8b86aad73bee1bf5e9e58cf7
Jefferson Eagley <jefferson.eagley@gmail.com>
2018-07-14 23:29:51 -0700

    repeat import

--- react/src/index.js
+++ react/src/index.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import {AppContainer} from 'react-hot-loader';
 import AppReduxProvider from './component/AppReduxProvider';
 import AppRouter from './component/AppRouter';
