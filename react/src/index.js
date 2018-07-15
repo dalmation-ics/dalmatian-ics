7c64df1e8b102e57019b8ea264765dbd6063e3c8
Jefferson Eagley <jefferson.eagley@gmail.com>
2018-07-14 23:26:32 -0700

    fix bootstrap

--- react/src/index.js
+++ react/src/index.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import {AppContainer} from 'react-hot-loader';
 import AppReduxProvider from './component/AppReduxProvider';
 import AppRouter from './component/AppRouter';
