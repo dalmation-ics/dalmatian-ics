6b483af90aa18a5d59d16012d6edb51d790c1d97
Jefferson Eagley <jefferson.eagley@gmail.com>
2018-07-17 18:03:44 -0700

    revamped settings legal to have state access and reworked for bs4

--- react/src/component/settingsPanel/component/settingsPanelComponent/settingLegal/index.js
+++ react/src/component/settingsPanel/component/settingsPanelComponent/settingLegal/index.js
@@ -1,60 +1,53 @@
 import React, {Component} from 'react';
 import thunkBindActionCreators from 'src/_core/redux/thunkBindActionCreators';
 import {connect} from 'react-redux';
-import {Alert, Button, FormGroup} from 'react-bootstrap';
+import {Button, FormGroup} from 'reactstrap';
 import {toast} from 'react-toastify';
 import LegalAgreement from 'src/_core/global/legal_agreement';
-import * as s from 'src/_core/res/strings';
 
 class SettingsLegal extends Component {
 
-    static onClick_ShowLegalAgreement = () => {
-        toast(({closeToast}) =>
-            <div>
-                <h3>Legal Agreement</h3>
-                <LegalAgreement/>
-                <button className='btn btn-primary'
-                        onClick={closeToast}>
-                    Close
-                </button>
-            </div>, {
-            position: toast.POSITION.TOP_CENTER,
-            autoClose: 60000,
-            closeButton: false,
-            draggable: false,
-            draggablePercent: 50,
-            type: toast.TYPE.INFO,
-            closeOnClick: false,
-        });
-    };
+  static onClick_ShowLegalAgreement = () => {
+    toast(({closeToast}) =>
+        <div>
+          <h3>Legal Agreement</h3>
+          <LegalAgreement/>
+          <Button color={'primary'}
+                  onClick={closeToast}>
+            Close
+          </Button>
+        </div>, {
+      position: toast.POSITION.TOP_CENTER,
+      autoClose: 60000,
+      closeButton: false,
+      draggable: false,
+      draggablePercent: 50,
+      type: toast.TYPE.INFO,
+      closeOnClick: false,
+    });
+  };
 
-    render() {
+  render() {
 
-        return (
-            <div className={'panel panel-primary'}>
-                <h3 className={'panel-heading'}>Legal</h3>
-                <FormGroup>
-                    <button
-                        onClick={SettingsLegal.onClick_ShowLegalAgreement}
-                    >
-                        Legal Agreement
-                    </button>
-                </FormGroup>
-            </div>
-        );
-    }
+    return (
+        <div className={'panel panel-primary'}>
+          <h3 className={'panel-heading'}>Legal</h3>
+          <FormGroup>
+            <button
+                onClick={SettingsLegal.onClick_ShowLegalAgreement}
+            >
+              Legal Agreement
+            </button>
+          </FormGroup>
+        </div>
+    );
+  }
 
 }
 
-export default SettingsLegal;
+const mapStateToProps = (state) => {
+  const {} = state;
+  return {};
+};
 
-// const mapStateToProps = (state) => {
-//     const {} = state;
-//     return {};
-// };
-//
-// const mapDispatchToProps = (dispatch) => {
-//     return thunkBindActionCreators({}, dispatch);
-// };
-//
-// export default connect(mapStateToProps, mapDispatchToProps)(SettingsLegal);
+export default connect(mapStateToProps)(SettingsLegal);
