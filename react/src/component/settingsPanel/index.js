4394f57e5f608768b507d1b1b9b1648bcb850ea1
Jefferson Eagley <jefferson.eagley@gmail.com>
2018-07-17 17:54:49 -0700

    exported settings core components

---
+++ react/src/component/settingsPanel/index.js
@@ -0,0 +1,6 @@
+// @flow
+import settingsButton from './component/settingsButton';
+import settingsModal from './component/settingsModal';
+
+export const SettingsButton = settingsButton;
+export const SettingsModal = settingsModal;
