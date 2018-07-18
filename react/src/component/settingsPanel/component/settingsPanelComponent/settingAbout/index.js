590909ebe889583a8a4231037c2b5b0ddd2bd1ea
Jefferson Eagley <jefferson.eagley@gmail.com>
2018-07-17 20:22:51 -0700

    redid about page

--- react/src/component/settingsPanel/component/settingsPanelComponent/settingAbout/index.js
+++ react/src/component/settingsPanel/component/settingsPanelComponent/settingAbout/index.js
@@ -1,28 +1,61 @@
 // @flow
 import React, {Component} from 'react';
+import {
+  Card,
+  CardTitle,
+  CardText,
+  CardBody,
+  CardDeck,
+  CardFooter,
+} from 'reactstrap';
+import DeveloperCardStack from './container/developerCardStack';
 
 class SettingAbout extends Component<{}> {
 
   render() {
     return (
-        <div className={'panel panel-primary'}>
-          <h3 className={'panel-heading'}>Credits</h3>
-          <div className={'panel-body'}>
-            <h4>Bellevue College BC ICS team</h4>
-            <p>
-              BC ICS was originally developed by Bellevue Colllege
-              students from the Bachelors of Applied Science in IST:
-              Application Development. It was developed as part of the
-              2018 graduating class' capstone in concert with Eastside
-              Fire & Rescure.
-            </p>
-            <h4>Graphics credits</h4>
-            <ul>
-              <li>Font Awesome</li>
-              <li>Bootstrap 3</li>
-            </ul>
-          </div>
-        </div>
+        <Card>
+          <CardTitle>About</CardTitle>
+          <CardBody>
+            <CardDeck>
+              <Card className={'col-xs-12 col-sm-8'}>
+                <CardTitle>Dalmatian ICS team</CardTitle>
+                <CardBody>
+                  <p>
+                    Dalmatian ICS is the continuation of BC_ICS, an Incident
+                    Command System form editor and manager. Originally designed
+                    for Eastside Fire & Rescue in Washington State, the utility
+                    of Dalmatian ICS extends to any group who rely on the
+                    creation and distribution of ICS forms.
+                  </p>
+                </CardBody>
+                <CardFooter>
+                  <CardText>Developers</CardText>
+                  <DeveloperCardStack/>
+                </CardFooter>
+              </Card>
+              <Card className={'col-xs-12 col-sm-4'}>
+                <CardTitle>Technical stack</CardTitle>
+                <CardBody>
+                  <dl>
+                    <dt>Dev platform:</dt>
+                    <dd> NodeJS (Javascript / ES6 / ES7 /
+                      ECMAScript)
+                    </dd>
+                    <dt>UI & State:</dt>
+                    <dd> ReactJS & ReduxJS</dd>
+                    <dt>OS frame:</dt>
+                    <dd> ElectronJS</dd>
+                    <dt>Icons:</dt>
+                    <dd> Font Awesome</dd>
+                    <dt>UX & style:</dt>
+                    <dd> Twitter bootstrap</dd>
+                  </dl>
+                </CardBody>
+              </Card>
+            </CardDeck>
+          </CardBody>
+        </Card>
     );
   }
 }
