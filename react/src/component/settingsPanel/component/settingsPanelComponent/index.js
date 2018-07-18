fb54355e05d1e1091d63f96eb5526c48fe14eb1e
Jefferson Eagley <jefferson.eagley@gmail.com>
2018-07-17 17:45:11 -0700

    added documentation to settings list

--- react/src/component/settingsPanel/component/settingsPanelComponent/index.js
+++ react/src/component/settingsPanel/component/settingsPanelComponent/index.js
@@ -3,8 +3,10 @@ import SettingAbout from './setting_About';
 import SettingLegal from './setting_Legal';
 import ThemeSelector from './theme_selector/component/settingTheme/index';
 
-// determines what settings items will be imported
+// select what settings items will actually be displayed by commenting them
+// out in the following export
 
+///List of settings items
 export default [
   SettingAbout,
   SettingLegal,
