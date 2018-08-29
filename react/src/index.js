7f20d8acb97b335e0cd9379d40756d511d8a03ad
Jefferson Eagley <jefferson.eagley@gmail.com>
2018-08-29 00:45:49 -0700

    electron initialize method needed called

--- react/src/index.js
+++ react/src/index.js
@@ -6,7 +6,15 @@ import {AppContainer} from 'react-hot-loader';
 import AppReduxProvider from './component/appReduxProvider';
 import AppRouter from './component/appRouter';
 import registerServiceWorker from './registerServiceWorker';
+import {initialize as electron_initialize} from 'src/_core/electron';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+/**
+ * Setup use of Electron in React
+ * electron_initialize() must be called at the start to allow
+ * utils/electron.js to return electron within the application.
+ */
+electron_initialize();
 
 const root: ?Element = document.getElementById('root');
 
