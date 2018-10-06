58757af429d953e565380d0fc6c130154004d5ea
Jefferson Eagley <jefferson.eagley@gmail.com>
2018-10-05 20:52:30 -0700

    rebuilt rename

--- reactui/src/component/appRouter/component/pageSuite/component/buttonSuiteItemRename/index.js
+++ reactui/src/component/appRouter/component/pageSuite/component/buttonSuiteItemRename/index.js
@@ -1,17 +1,14 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {FormGroup, Modal} from 'reactstrap';
+import uuid from 'uuid';
 
-import PropTypes from 'prop-types';
-import action_Archive_Suite_Rename
+import action_Archive_Item_Rename
   from 'src/_redux/action/action_Archive_Item/action_Archive_Item_Rename';
 import thunkBindActionCreators from 'src/_core/redux/thunkBindActionCreators';
-import {
-  CommandBarItemNav,
-  CommandBarItemAction,
-} from 'src/component/global/commandBar/component/commandBarItem';
-import uuid from 'uuid';
-import type {ActionBound} from '../../../../../../_core/redux/types';
+import ToastPromptButton from 'src/component/global/toastPromptButton';
+import * as s from 'src/_core/res/strings';
+
+import type {ActionBound} from 'src/_core/redux/types';
 
 type props = {
   archive: Array<any> | null,
@@ -24,23 +21,17 @@ class SuiteRename extends Component<props> {
   constructor(props, context) {
     super(props, context);
 
-    this.handleShow = this.handleShow.bind(this);
-    this.handleClose = this.handleClose.bind(this);
-    this.updateInputValue = this.updateInputValue.bind(this);
-
     this.state = {
-      show: false,
       newName: '',
     };
   }
 
-  handleClose() {
-    this.setState({show: false});
-  }
+  onClick_renameFile = () => {
+    const {action_Archive_Item_Rename} = this.props;
+    let newName = this.state.newName.trim();
 
-  handleShow() {
-    this.setState({show: true});
-  }
+    action_Archive_Item_Rename(newName);
+  };
 
   updateInputValue = (e) => {
     this.setState({newName: e.target.value});
@@ -48,22 +39,30 @@ class SuiteRename extends Component<props> {
 
   addSpace = (e) => {
     const key = e.which;
-    if (key == 32) {
+    if (key === 32) {
       let val = this.state.newName;
       val += ' ';
       this.setState({newName: val});
     }
   };
 
+  onChange_updateNewName = (e) => {
+    this.addSpace(e);
+    this.updateInputValue(e);
+  };
+
   render() {
     let {archive, suiteSelectedUUID} = this.props.archiveStore;
     let fileName = archive.find(f => f.uuid === suiteSelectedUUID).fileName;
 
-    let {
-      action_Archive_Suite_Rename,
-    } = this.props;
-
-    return <div></div>;
+    return <ToastPromptButton action={this.onClick_renameFile}
+                              textButtonTitle={s.ARCHIVE.ITEM.RENAME}>
+      <div>
+        <p>Are you sure you want to delete {fileName}</p>
+        <input onKeyDown={this.onChange_updateNewName}
+               onChange={this.onChange_updateNewName}/>
+      </div>
+    </ToastPromptButton>;
   }
 
 }
@@ -77,7 +76,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return thunkBindActionCreators({
-    action_Archive_Suite_Rename,
+    action_Archive_Item_Rename,
   }, dispatch);
 };
 
