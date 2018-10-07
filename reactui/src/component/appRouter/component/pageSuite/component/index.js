1e6ba9d0e1decbc0e92bb601c0fe17ad0906a159
Daryk Kohler <daryk.kohler@gmail.com>
2018-10-07 01:49:53 -0700

    stop spilling typescript on the flow stuff Jeff

--- reactui/src/component/appRouter/component/pageSuite/component/index.js
+++ reactui/src/component/appRouter/component/pageSuite/component/index.js
@@ -1,6 +1,5 @@
-'use strict';
-Object.defineProperty(exports, '__esModule', {value: true});
-var buttonSuiteItemRename_1 = require('./buttonSuiteItemRename');
-exports.ButtonSuiteItemRename = buttonSuiteItemRename_1.default;
-var buttonSuiteItemDelete_1 = require('./buttonSuiteItemDelete');
-exports.ButtonSuiteItemDelete = buttonSuiteItemDelete_1.default;
+import buttonSuiteItemRename_1 from './buttonSuiteItemRename';
+import buttonSuiteItemDelete_1 from './buttonSuiteItemDelete';
+
+export const ButtonSuiteItemRename = buttonSuiteItemRename_1;
+export const ButtonSuiteItemDelete = buttonSuiteItemDelete_1;
--- reactui/src/component/appRouter/component/pageSuite/component/index.ts
+++
@@ -1,2 +0,0 @@
-export {default as ButtonSuiteItemRename} from './buttonSuiteItemRename'
-export {default as ButtonSuiteItemDelete} from './buttonSuiteItemDelete'
