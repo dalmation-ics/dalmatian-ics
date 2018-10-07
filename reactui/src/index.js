dcb2c9293a3d3f08998d10fb242c6925662140b2
Daryk Kohler <daryk.kohler@gmail.com>
2018-10-07 02:10:06 -0700

    move @flow to top of page

--- reactui/src/index.js
+++ reactui/src/index.js
@@ -1,5 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 // @flow
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {AppContainer} from 'react-hot-loader';
