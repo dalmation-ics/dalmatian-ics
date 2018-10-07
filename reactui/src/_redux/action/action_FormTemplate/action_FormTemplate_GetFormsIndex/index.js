084de9aef556cbe6117669e189c1bbb22baa9a21
Daryk Kohler <daryk.kohler@gmail.com>
2018-10-07 01:57:30 -0700

    move @flow to top of page

--- reactui/src/_redux/action/action_FormTemplate/action_FormTemplate_GetFormsIndex/index.js
+++ reactui/src/_redux/action/action_FormTemplate/action_FormTemplate_GetFormsIndex/index.js
@@ -1,6 +1,7 @@
+// @flow
+
 import {ACT_GET_FORMS_INDEX} from 'src/_core/contract/formsBridge';
 import ipcRWrapper from 'src/_core/electron/ipcWrapper/index';
-// @flow
 import type {Action, Dispatch} from 'src/_core/redux/types';
 import actionStatus from 'src/_core/redux/types/actionStatus';
 
