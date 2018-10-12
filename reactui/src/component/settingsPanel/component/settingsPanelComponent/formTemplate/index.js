afbbbc46b60550181b7483520346fe55983f6333
Jefferson Eagley <jefferson.eagley@gmail.com>
2018-10-12 15:20:03 -0700

    base file for form template upgrade

---
+++ reactui/src/component/settingsPanel/component/settingsPanelComponent/formTemplate/index.js
@@ -0,0 +1,34 @@
+// @flow
+import * as React from 'react';
+import {Component} from 'react';
+import {connect} from 'react-redux';
+import {Card, CardBody, CardHeader, CardTitle,} from 'reactstrap';
+
+class formTemplateConfig extends Component<{}> {
+
+	render() {
+		return (
+			<div>
+				<Card>
+					<CardHeader>
+						<CardTitle>Form template settings</CardTitle>
+					</CardHeader>
+					<CardBody>
+						<p>
+							Form repo selector (chooses where to download forms from)
+						</p>
+						<p>
+							Form download selector (chooses what forms to download)
+						</p>
+					</CardBody>
+				</Card>
+			</div>
+		);
+	}
+}
+
+const mapStateToProps = () => {
+	return {};
+};
+
+export default connect(mapStateToProps)(formTemplateConfig);
